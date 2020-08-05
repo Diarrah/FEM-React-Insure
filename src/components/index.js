6a5e69a7ab7e4f2bcbb96e2b51f086ee590f2d21
Diarrah <diarrahcasiano@gmail.com>
2020-08-04 21:51:46 -0700

    All components grouped for easy import

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+  
+export { default as Navbar } from './Navbar';
+export { default as Header } from './Header';
+export { default as Different } from './Different';
+export { default as CTA } from './CTA';
+export { default as Footer } from './Footer';
