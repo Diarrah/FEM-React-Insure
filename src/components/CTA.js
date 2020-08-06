e1c8064fafc272cc3f394d0e416512a977a083c0
Diarrah <diarrahcasiano@gmail.com>
2020-08-05 23:50:57 -0700

    -

---
+++ src/components/CTA.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CTA = () => (
+    <div className="cta">
+        <h1 className="cta__heading">Find out more about how we work</h1>
+        <button className="btn btn--gray">How we work</button>
+    </div>
+)
+
+export default CTA;
