41537408dc2a7d529f2710260e026451df71ec33
Diarrah <diarrahcasiano@gmail.com>
2020-08-06 01:38:58 -0700

    All components import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './App.scss';
+
+import { Navbar, Header, Different, CTA, Footer } from './components';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Header />
+      <Different />
+      <CTA />
+      <Footer />
     </div>
   );
 }
