654f97c4390417de551b4db3ef8130443658a5a6
Diarrah <diarrahcasiano@gmail.com>
2020-08-08 16:18:25 -0700

    Added textbox div as wrapper to h3 + p tag

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,10 @@ import React from 'react';
 const DifferentAttribute = ({ icon, heading, subheading }) => (
     <div className="different__attribute__card">
         <img className="different__attribute__card__icon" src={icon} alt="" />
-        <h3 className="different__attribute__card__heading">{heading}</h3>
-        <p className="different__attribute__card__subheading">{subheading}</p>
+        <div className="different__attribute__card__textbox">
+            <h3 className="different__attribute__card__heading">{heading}</h3>
+            <p className="different__attribute__card__subheading">{subheading}</p>
+        </div>
     </div>
 )
 
